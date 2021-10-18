fa67791dd86b0380a9d5299cd036982178930c30
YijunBao <yijun.bao@duke.edu>
2021-10-18 11:59:19 -0400

    Changed some names

--- plot_figures/plot_figures_all.m
+++ plot_figures/plot_figures_all.m
@@ -12,10 +12,10 @@ traces_1p_for_Fig4A            % Compare the unmixed traces of one-photon videos
 traces_1p_Fig4A                % Plot the example unmixed traces of a one-photon video in Fig 4A
 frame_1p_Fig4A                 % Plot the example peak frames of a one-photon video in Fig 4A
 F1_time_all_Fig2_4             % Plot the F1 and processing time in Fig 2C-D, Fig 3B-C, and Fig 4B-C
-max_projection_S2Fig           % Plot the maximum projection images of four videos in S2 Fig
-traces_alpha_for_S3Fig3A       % Compare the unmixed traces of ABO videos using different fixed alpha. Followed by "traces_alpha_S3Fig3A"
-traces_alpha_S3Fig3A           % Plot the example unmixed traces of ABO videos using different fixed alpha in S3 Fig A
-curves_alpha_S3FigB_E          % Plot the F1 and processing speed with differnt fixed alpha in S3 Fig B-E
-time_ABO_full_S4Fig            % Plot the processing time on full ABO videos in S4 Fig
-F1_time_bin_S5Fig              % Plot the F1 and processing time with different downsampling ratios in S5 Fig
-F1_time_all_param_NAOMi_S6Fig  % Plot the F1 and processing time for NAOMi videos with different simulation parameters in S6 Fig
+max_projection_FigS2           % Plot the maximum projection images of four videos in Fig S2
+traces_alpha_for_FigS3A       % Compare the unmixed traces of ABO videos using different fixed alpha. Followed by "traces_alpha_FigS3A"
+traces_alpha_FigS3A           % Plot the example unmixed traces of ABO videos using different fixed alpha in Fig S3 A
+curves_alpha_FigS3B_E          % Plot the F1 and processing speed with differnt fixed alpha in Fig S3B-E
+time_ABO_full_FigS4            % Plot the processing time on full ABO videos in Fig S4
+F1_time_bin_FigS5              % Plot the F1 and processing time with different downsampling ratios in Fig S5
+F1_time_all_param_NAOMi_FigS6  % Plot the F1 and processing time for NAOMi videos with different simulation parameters in Fig S6
